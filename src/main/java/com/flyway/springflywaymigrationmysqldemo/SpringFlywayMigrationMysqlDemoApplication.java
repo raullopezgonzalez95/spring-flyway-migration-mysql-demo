656ddd3cbc180f1a3b65546a7b2530271052058b
Raul Lopez <raul.lopez@meltsan.com>
2024-02-21 13:05:01 -0600

    proyecto inicial

---
+++ src/main/java/com/flyway/springflywaymigrationmysqldemo/SpringFlywayMigrationMysqlDemoApplication.java
@@ -0,0 +1,13 @@
+package com.flyway.springflywaymigrationmysqldemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFlywayMigrationMysqlDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringFlywayMigrationMysqlDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/flyway/springflywaymigrationmysqldemo/SpringFlywayMigrationMysqlDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flyway.springflywaymigrationmysqldemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringFlywayMigrationMysqlDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
